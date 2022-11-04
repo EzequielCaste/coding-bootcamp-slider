e67e9ed155bdc86e9101d597cb5aca01b1fdd76a
EzequielCaste <ezequiel.caste@gmail.com>
2022-11-04 12:00:22 -0300

    fix mobile slider

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-// hide the other slide
-
-const slides = document.querySelectorAll('.slide-container');
-
-//slides[1].setAttribute('style', 'transform: translate(200%)');
-
+const slide1 = document.getElementById("slide1");
+const slide2 = document.getElementById("slide2");
 
 const prevBtn = document.querySelectorAll('.fa-chevron-left')
 const nextBtn = document.querySelectorAll('.fa-chevron-right')
 
-prevBtn.forEach( btn => btn.addEventListener('click', prevSlide))
-nextBtn.forEach( btn => btn.addEventListener('click', nextSlide))
+prevBtn.forEach( btn => btn.addEventListener('click', changeSlide))
+nextBtn.forEach( btn => btn.addEventListener('click', changeSlide))
 
-function prevSlide() {
-  slides[0].setAttribute('style', 'transform: translate(0%)');
-  slides[1].setAttribute('style', 'transform: translate(400%)');
-}
-function nextSlide() {
-  slides[0].setAttribute('style', 'transform: translate(400%)');
-  slides[1].setAttribute('style', 'transform: translate(-300%)');
+function changeSlide() {
+
+  if (slide1.style.transform === 'translate(-120%)') {
+    slide1.setAttribute('style', 'visibility: visible;transform: translate(0%)');
+    slide2.setAttribute('style', 'visibility: visible;transform: translate(50%)');
+
+  } else {
+    slide1.setAttribute('style', 'transform: translate(-120%)');
+    slide2.setAttribute('style', 'visibility: visible;transform: translate(-100%)');
+    
+  }
 }
